454c356eaa4f6d59d0ed7848aaab750f580b4ced
xavi-burgos99 <xavi.burgos99@gmail.com>
2025-03-15 17:49:49 +0100

    Full fix

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -5,7 +5,7 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
+    return const Center(
       child: Text('Home Screen', style: TextStyle(fontSize: 24)),
     );
   }
--- lib/map_screen.dart
+++ lib/map_screen.dart
@@ -5,7 +5,7 @@ class MapScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
+    return const Center(
       child: Text('Map Screen', style: TextStyle(fontSize: 24)),
     );
   }
